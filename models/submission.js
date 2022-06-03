f3ea370f4d95158cba42f6dfa1cf854b4896b44c
Randy-Osborn <randyosborn1989@hotmail.com>
2022-06-03 14:08:19 -0700

    Implement some starter code for the assignments.

--- models/submission.js
+++ models/submission.js
@@ -1,13 +1,21 @@
-const { ObjectId } = require('mongodb')
+const { ObjectId } = require("mongodb");
 
-const { getDbReference } = require('../lib/mongo')
-const { extractValidFields } = require('../lib/validation')
+const { getDbReference } = require("../lib/mongo");
+const { extractValidFields } = require("../lib/validation");
 
 const submissionSchema = {
-    assignmentId: { required: true },
-    studentId: { required: true },
-    timestamp: { required: true },
-    grade: { required: true },
-    file: {required: true}
-}
+  assignmentId: { required: true },
+  studentId: { required: true },
+  timestamp: { required: true },
+  grade: { required: true },
+  file: { required: true },
+};
 
+exports.insertNewSubmission = async function insertNewSubmission(submission) {
+  const db = getDbReference();
+  const collection = db.collection("submissions");
+
+  user = extractValidFields(submission, submissionSchema);
+  const result = await collection.insertOne(submission);
+  return result.insertedId;
+};
