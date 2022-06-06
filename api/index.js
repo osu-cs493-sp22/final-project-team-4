0b96a95ffbd5094e2dd3cf6784653c44f098d953
Randy-Osborn <45839750+Randy-Osborn@users.noreply.github.com>
2022-06-05 18:45:17 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 
 const router = Router()
 
-
 router.use('/assignments', require('./assignments'))
 router.use('/courses', require('./courses'))
 //router.use('/submissions', require('./submissions'))
