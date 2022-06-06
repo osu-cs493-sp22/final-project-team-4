69dbe2dc17e0af34839d7dd6c2d5755c725abe98
Josh Minyard <joshdaminyard@gmail.com>
2022-06-05 19:45:31 -0700

    made initdb things have int ids

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ const router = Router()
 
 router.use('/assignments', require('./assignments'))
 router.use('/courses', require('./courses'))
-//router.use('/submissions', require('./submissions'))
 router.use('/users', require('./users'))
 
 module.exports = router
