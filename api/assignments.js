f4e0ad0a4664e08ff1a48eee4e9dc72d47db6bce
EmmaLi1 <43556456+EmmaLi1@users.noreply.github.com>
2022-06-07 13:05:19 -0700

    assignment endpoints

--- api/assignments.js
+++ api/assignments.js
@@ -2,8 +2,14 @@ const { Router } = require('express')
 const { getDbReference } = require('../lib/mongo')
 
 const { validateAgainstSchema } = require('../lib/validation')
+<<<<<<< Updated upstream
 const { insertNewAssignment, getAssignmentById, assignmentSchema, getAssignmentAndSubmissionById } = require('../models/assignment')
 const{submissionSchema,insertNewSubmission} = require('../models/submission')
+=======
+const { insertNewAssignment, getAssignmentById, assignmentSchema,patchAssignmentById,deleteAssignmentById, getAssignmentAndSubmissionById, isStudentAndAssignmentInCourse } = require('../models/assignment')
+const { submissionSchema, insertNewSubmission, savePhotoFile, getSubmissionsPage, isUserInstructorOfCourse } = require('../models/submission')
+const { requireAuthentication, isUserStudent, isUserInstructor } = require('../lib/auth')
+>>>>>>> Stashed changes
 const router = Router()
 
 router.post('/', async function (req, res, next) {
@@ -33,13 +39,63 @@ router.get('/:courseid', async function (req, res, next)  {
     }
 })
 router.patch('/:courseid', async function (req, res, next) {
-    const id = req.params.id
-    res.status(200).send({})
+    const courseid = parseInt(req.params.courseid);
+    if (await isUserAdmin(req.user) || await isUserInstructorOfCourse(req.user, courseid)) {
+        try {
+            const asssignment = await getAssignmentById(courseid)
+            if (asssignment) {
+                const result = patchAssignmentById(courseid, req.body)
+                if (result) {
+                    res.status(200).send();
+                } else {
+                    res.status(400).send({
+                        error: "The request body was either not present or did not contain any fields related to Assignment objects."
+                    })
+                }
+            } else {
+                next()
+            }
+        } catch (err) {
+            console.error(err)
+            res.status(400).send({
+                error: "The request body was either not present or did not contain any fields related to Assignment objects."
+            })
+        }
+    } else {
+        res.status(403).send({ err: "request not made by authenticated user" })
+    }
 })
 
-router.delete('/:courseid', async function (req, res, next) {
-    const id = req.params.id
-    res.status(204).send()
+router.delete('/:courseid', requireAuthentication,async function (req, res, next) {
+    const courseid = parseInt(req.params.courseid);
+    const asssignment = await getAssignmentById(courseid)
+    if(asssignment){
+        if(await isUserAdmin(req.user) || await isUserInstructorOfCourse(req.user, courseid)){
+            try {
+                const asssignment = await getAssignmentById(courseid)
+                if (asssignment) {
+                    if (await deleteAssignmentById(courseid)) {
+                        res.status(204).send();
+                    } else {
+                        res.status(500).send({
+                            error: "Unable to delete asssignment."
+                        })
+                    }
+                } else {
+                    next()
+                }
+            } catch (err) {
+                console.error(err)
+                res.status(500).send({
+                    error: "Unable to delete asssignment. Please try again later."
+                })
+            } 
+        } else {
+            res.status(403).send({ err: "request not made by authenticated user" })
+        }
+    }else{
+        next()
+    }
 })
 
 router.get('/:courseid/submissions', async function (req, res, next)  {
